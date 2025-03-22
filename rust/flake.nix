9ac7cda75b2a71c3976f179f25e44697abe8483b
Quirin Brändli <qbraendli@pm.me>
2025-03-22 02:22:11 +0100

    Nixfmt

--- rust/flake.nix
+++ rust/flake.nix
@@ -9,79 +9,56 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      rust-overlay,
-    }:
+  outputs = { self, nixpkgs, rust-overlay, }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems (system:
           f {
             pkgs = import nixpkgs {
               inherit system;
-              overlays = [
-                rust-overlay.overlays.default
-                self.overlays.default
-              ];
+              overlays =
+                [ rust-overlay.overlays.default self.overlays.default ];
             };
-          }
-        );
-    in
-    {
+          });
+    in {
       overlays.default = final: prev: {
-        rustToolchain =
-          let
-            rust = prev.rust-bin;
-          in
-          if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = [
-                "rust-src"
-                "rustfmt"
-              ];
-            };
+        rustToolchain = let rust = prev.rust-bin;
+        in if builtins.pathExists ./rust-toolchain.toml then
+          rust.fromRustupToolchainFile ./rust-toolchain.toml
+        else if builtins.pathExists ./rust-toolchain then
+          rust.fromRustupToolchainFile ./rust-toolchain
+        else
+          rust.stable.latest.default.override {
+            extensions = [ "rust-src" "rustfmt" ];
+          };
       };
 
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              rustToolchain
-              openssl
-              pkg-config
-              cargo-deny
-              cargo-edit
-              cargo-watch
-              rust-analyzer
-            ];
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            openssl
+            pkg-config
+            cargo-deny
+            cargo-edit
+            cargo-watch
+            rust-analyzer
+          ];
 
-            shellHook = ''
-              # Create a symlink for clang
-              mkdir -p .bins/rustToolchain
-              ln -s ${pkgs.rustToolchain}/bin .bins/rustToolchain
-            '';
+          shellHook = ''
+            # Create a symlink for clang
+            mkdir -p .bins/rustToolchain
+            ln -s ${pkgs.rustToolchain}/bin .bins/rustToolchain
+          '';
 
-            env = {
-              # Required by rust-analyzer
-              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
-            };
+          env = {
+            # Required by rust-analyzer
+            RUST_SRC_PATH =
+              "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
           };
-        }
-      );
+        };
+      });
     };
 }
